fafcbd13f871d2a862f67a36fd9ffd6966003af1
Important-BFX <138168189+Important-BFX@users.noreply.github.com>
2023-08-02 19:33:48 -0300

    Add files via upload

---
+++ ScripLuis.lua
@@ -0,0 +1,23 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "SukHubb", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+OrionLib:MakeNotification({
+	Name = "Welcome!",
+	Content = "Thanks For Using Suk Hubb",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+
+local MM2 = Window:MakeTab({
+	Name = "Murder Mystrey 2",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+MM2:AddButton({
+	Name = "OP GUI (patched)",
+	Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Doggo-cryto/EclipseMM2/master/Script", true))()
+      		print("Loaded")
+  	end    
+})
